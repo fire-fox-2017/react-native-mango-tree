08c1d8e7e3ad17d87071181c7a26ad77bb0f454e
Bill Ivan Kooslarto <billivank_96@yahoo.co.id>
2017-06-05 15:30:35 +0700

    checkpoint 3

--- mangotree/src/components/Main.js
+++ mangotree/src/components/Main.js
@@ -2,19 +2,40 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { View, Text, Button } from 'react-native';
 
-import { growTree } from '../actions'
+import { growTree, harvestTree } from '../actions'
 
 class Main extends Component {
+  constructor(props) {
+    super(props)
+    this.state= {
+      died: false
+    }
+  }
+
+  grow() {
+    this.props.growTree(this.props.tree)
+    if(this.props.tree.age === this.props.tree.maximum_age){
+      const { navigate } = this.props.navigation
+      navigate('Over')
+      this.setState({ died: true })
+    }
+  }
+
   render() {
     return (
       <View>
-        <Text>{this.props.tree.name}</Text>
-        <Text>{this.props.tree.age}</Text>
-        <Text>{this.props.tree.fruit}</Text>
-        <Text>{this.props.tree.harvested}</Text>
+        <Text>name: {this.props.tree.name}</Text>
+        <Text>age: {this.props.tree.age}</Text>
+        <Text>fruit: {this.props.tree.fruit}</Text>
+        <Text>harvested: {this.props.tree.harvested}</Text>
         <Button
           title='Grow!'
-          onPress={()=>this.props.growTree(this.props.tree)}
+          onPress={()=>this.grow()}
+          disabled={this.state.died}
+        />
+        <Button
+          title='Harvest!'
+          onPress={()=>this.props.harvestTree(this.props.tree)}
         />
       </View>
     )
@@ -28,6 +49,9 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
   growTree: (tree) => {
     dispatch(growTree(tree))
+  },
+  harvestTree: (tree) => {
+    dispatch(harvestTree(tree))
   }
 })
 
--- mangotree/src/reducers/index.js
+++ mangotree/src/reducers/index.js
@@ -10,18 +10,48 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionType.NEW_TREE:
-      return {...state, name: action.details.name, owner: action.details.owner}
+      const newState = {}
+      newState.age = initialState.age
+      newState.fruit = initialState.fruit
+      newState.harvested = initialState.harvested
+      newState.maximum_age = initialState.maximum_age
+      newState.name = action.details.name
+      newState.owner = action.details.owner
+      return newState
     case actionType.GROW_TREE:
       if(action.tree.age < 0.6 * state.maximum_age){
-        action.tree.age += 1;
-        return action.tree;
+        const newState = {}
+        newState.age = state.age
+        newState.fruit = state.fruit
+        newState.harvested = state.harvested
+        newState.maximum_age = state.maximum_age
+        newState.name = state.name
+        newState.owner = state.owner
+        newState.age += 1;
+        return newState;
       } else if ( action.tree.age > state.maximum_age){
         return initialState;
       } else {
-        action.tree.age += 1;
-        action.tree.fruit += Math.floor(Math.random() * (7)) + 1;
-        return action.tree;
+        const newState = {}
+        newState.age = state.age
+        newState.fruit = state.fruit
+        newState.harvested = state.harvested
+        newState.maximum_age = state.maximum_age
+        newState.name = state.name
+        newState.owner = state.owner
+        newState.age += 1;
+        newState.fruit += Math.floor(Math.random() * (7)) + 1;
+        return newState;
       }
+    case actionType.HARVEST_TREE:
+      const newState2 = {}
+      newState2.age = state.age
+      newState2.fruit = 0
+      newState2.harvested = state.fruit + state.harvested
+      newState2.maximum_age = state.maximum_age
+      newState2.name = state.name
+      newState2.owner = state.owner
+      return newState2
     default:
       return state;
   }
