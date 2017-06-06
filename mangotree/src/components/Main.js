8f861ea684aa97b04630877f32eb2d97c292eb55
Bhirawa Mbani <bhirmbani@gmail.com>
2017-06-06 07:46:06 +0700

    adding tree img and basic grow

--- mangotree/src/components/Main.js
+++ mangotree/src/components/Main.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { Text, View, TextInput, Button, Image } from 'react-native';
+import { connect } from 'react-redux';
 
+import { growSuccess, harvestSuccess } from '../actions';
 
 class MainScreen extends Component {
   static navigationOptions = {
@@ -14,17 +16,88 @@ class MainScreen extends Component {
     };
   }
 
-  
+  checkFruit(fruit) {
+    if (fruit !== 0) {
+      this.setState({
+        toggleHarvest: false
+      })
+    } else {
+      this.setState({
+        toggleHarvest: true
+      })
+    }
+  }
+
+  growTree = () => {
+    this.props.growSuccess();
+    this.checkFruit(this.props.tree.fruit);
+    if(this.props.tree.status === 'dead') {
+      const { navigate } = this.props.navigation;
+      navigate('GameOver');
+    } else if (this.props.tree.age === Math.floor(this.props.tree.harvestAge * (1 / 3))) {
+      this.setState({
+        growthStats: 1
+      });
+    } else if (this.props.tree.age === Math.floor(this.props.tree.harvestAge * (2 / 3))) {
+      this.setState({
+        growthStats: 2
+      });
+    } else if (this.props.tree.age === this.props.tree.harvestAge) {
+      this.setState({
+        growthStats: 3
+      });
+    }
+  }
+
+  harvestTree = () => {
+    this.props.harvestSuccess();
+    this.checkFruit(this.props.tree.fruit);
+  }
 
   render() {
+    let img = require(`../../assets/0.png`);
+    if (this.state.growthStats === 1) {
+      img = require(`../../assets/1.png`);
+    } else if (this.state.growthStats === 2) {
+      img = require(`../../assets/2.png`);
+    } else if (this.state.growthStats === 3) {
+      img = require(`../../assets/3.png`);
+    }
+
     return (
       <View>
-        <Text>Main Component</Text>
-        <TouchableOpacity onPress={() => this.props.navigation.navigate('GameOver')} style={{ backgroundColor: 'green' }}><Text>Emulate</Text></TouchableOpacity>
-        <TouchableOpacity onPress={() => this.props.navigation.navigate('GameOver')} style={{ backgroundColor: 'green' }}><Text>Gameover</Text></TouchableOpacity>
+        <Text>Hello, {this.props.tree.name}!</Text>
+        <Text>This is "{this.props.tree.plantName}", your tree.</Text>
+        <Text>He is now {this.props.tree.age} {this.props.tree.age > 1 ? 'years' : 'year'} old.</Text>
+        <Text>Fruits harvested: {this.props.tree.harvest}</Text>
+        <Image source={img} />
+        <Button
+          onPress={() => this.growTree()}
+          title="Emulate"
+          color="green"
+        />
+        <Button
+          onPress={() => this.harvestTree()}
+          title="Harvest"
+          disabled={this.state.toggleHarvest}
+          color="green"
+        />
       </View>
     );
   }
 }
 
-export default MainScreen
+const mapStateToProps = state => ({
+  tree: state.tree
+})
+
+const mapDispatchToProps = dispatch => ({
+  harvestSuccess: () => {
+    dispatch(harvestSuccess());
+  },
+  growSuccess: () => {
+    dispatch(growSuccess());
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainScreen);
